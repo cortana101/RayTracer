6dbc4ac6fb48bddd97ceb5c847da50a72445293e
cortana101 <shihdan@hotmail.com>
2014-05-18 17:22:27 -0700

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Daniel Shih on 5/18/14.
+//  Copyright (c) 2014 Daniel Shih. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
